e887270322fb8a949b322f445d01d1b48f6bfca8
328877098 <328877098@qq.com>
2012-02-28 14:40:23 +0000

    添加一个list.php啦

---
+++ function.php
@@ -0,0 +1,47 @@
+<?php
+function add_slashes($string, $force = 1) {
+	if(is_array($string)) {
+		$keys = array_keys($string);
+		foreach($keys as $key) {
+			$val = $string[$key];
+			unset($string[$key]);
+			$string[addslashes($key)] = add_slashes($val, $force);
+		}
+	} else {
+		$string = addslashes($string);
+	}
+	return $string;
+}
+
+// 浏览器友好的变量输出
+function D($var, $echo=true, $label=null, $strict=true) {
+    $label = ($label === null) ? '' : rtrim($label) . ' ';
+    if (!$strict) {
+        if (ini_get('html_errors')) {
+            $output = print_r($var, true);
+            $output = "<pre>" . $label . htmlspecialchars($output, ENT_QUOTES) . "</pre>";
+        } else {
+            $output = $label . print_r($var, true);
+        }
+    } else {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        if (!extension_loaded('xdebug')) {
+            $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        }
+    }
+    if ($echo) {
+        echo($output);
+        return null;
+    }else
+        return $output;
+}
+
+function get_id($arg) {
+	 $id = isset($_REQUEST[$arg]) ? intval($_REQUEST[$arg]) : 1;
+	 return $id===0 ? 1 : $id;
+}
+
+?>
