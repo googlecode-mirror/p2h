a4b42dc9dff914ad6e17dbcda13d7f85a83a0d87
328877098 <328877098@qq.com>
2012-03-23 07:58:41 +0000

    del

--- P2H/update.js
+++
@@ -1,5 +0,0 @@
-/*
-This file is used to update html files, which is  trigger from user.
-update.js is included by templates,when user visit a template file,
-update.js will ask php to update corresponding html file when it's lose efficacy
-*/
--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('ROOT', __DIR__);
-define('DS', DIRECTORY_SEPARATOR);
-require_once './function/common.php';
-?>
