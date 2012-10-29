254f7eb5de2add11a58402360290a39b4aef828a
328877098 <328877098@qq.com>
2012-10-29 02:23:48 +0000

    ADD P2H

--- demo/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once './bootstrap.php';
-P2H::init();
-
-$time = date('Y-m-d H:i:s');
-//$var = '如果是字符串 不更新哦亲';
-//P2H::checkVar($var);
-include './templates/list.html';
-P2H::toHTML();
